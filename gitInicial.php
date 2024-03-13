8862bbd4580267a0ec9ca0ba1dcc83d0f801dbca
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-12 22:59:10 -0300

    Primeiro passo

---
+++ gitInicial.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Github Tutorial</title>
+</head>
+<body>
+    <?php 
+    
+    //1. Criação de um novo repositório local (Usa-se apenas na primeira vez que criou o repositório)
+    //Como criar meu git
+    //Primeiro passo: git init
+    //Segundo passo: git remote add origin link do repositório
+    //Terceiro passo: git branch -M main
+    //Quarto passo: git add .
+    //Quinto passo: git commit -m "Primeiro commit"
+    //Sexto passo: git push -u origin main
+
+    // 2. Novos commits
+    // Primeiro: git add .
+    // Segundo: git commit -m "Outro commit"
+    // Terceiro: git push
+
+    // 3. Clonagem do repositório em outro computador
+    // Primeiro: git clone link do repositório
+    // Segundo: git add .
+    // Terceiro: commit m- "Outro commit"
+    // Quarto: git push
+
+    // 4. Manipulação de Branchs
+    // git branch --list //Listar
+    // git branch <nova branch> //Criar
+    // git branch –d <branch> //Apagar
+    // git checkout <branch> //Alternar
+
+    ?>
+</body>
+</html>
