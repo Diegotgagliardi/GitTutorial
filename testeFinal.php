7a5bde2bc0d94fa34225cde45f1f1eff85acdc4f
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-12 23:26:44 -0300

    Final

---
+++ testeFinal.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    //Agora vou abrir outro repositório e voltar aqui para ver se consigo fazer o commit de forma correta:
+    //Duas horas depois kkk
+    //Foi :)
+    
+    ?>
+</body>
+</html>
