1f140d74642db63a5cf104152c968c82d255cc44
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-12 23:01:28 -0300

    Commits

---
+++ gitCommit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commits</title>
+</head>
+<body>
+    <?php 
+    
+    //Testando os commit para verificar como funciona
+    // 2. Novos commits
+    // Primeiro: git add .
+    // Segundo: git commit -m "Outro commit"
+    // Terceiro: git push
+    
+    ?>
+</body>
+</html>
